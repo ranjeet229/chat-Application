568f2569e6e33c45e1852d564f8225f45a3e081f
Ranjeet <kiranjeetkr80@gmail.com>
2025-10-07 01:05:31 +0530

    creating model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,30 +1,33 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePhoto: {
+      type: String,
+      default: "",
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+    },
   },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  profilePhoto: {
-    type: String,
-    default: "",
-  },
-  gender: {
-    type: String,
-    enum: ["male", "female", "other"],
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 export const User = mongoose.model("User", userSchema);
