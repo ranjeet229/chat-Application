f72e0df8d5f48caee065b863d54590900819dbc4
Ranjeet <kiranjeetkr80@gmail.com>
2025-10-08 01:04:23 +0530

    setup frontend dependency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+
+import Signup from './components/Signup';
 import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<HomePage/>
+  },
+  {
+    path:"/register",
+    element:<Signup/>
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  },
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <RouterProvider router={router}/>
     </div>
   );
 }
 
 export default App;
+
