fe0af2b6c7f9e6691b716e5635344b158d1c5a76
Ranjeet <kiranjeetkr80@gmail.com>
2025-10-14 06:47:17 +0530

    final

--- frontend/src/hooks/useGetOtherUsers.jsx
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -1,32 +1,26 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react'
+import axios from "axios";
+import { useDispatch } from "react-redux";
 import { setOtherUsers } from '../redux/userSlice';
 
 const useGetOtherUsers = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    const fetchOtherUsers = async () => {
-      try {
-        const token = localStorage.getItem("token");
-        const res = await axios.get(
-          "https://chat-application-1-w46p.onrender.com/api/v1/user/",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-            withCredentials: true,
-          }
-        );
-        dispatch(setOtherUsers(res.data));
-      } catch (error) {
-        console.error("Error fetching other users:", error.response?.data || error);
-      }
-    };
+    useEffect(() => {
+        const fetchOtherUsers = async () => {
+            try {
+                axios.defaults.withCredentials = true;
+                const res = await axios.get(`https://chat-application-1-w46p.onrender.com/api/v1/user`);
 
-    fetchOtherUsers();
-  }, [dispatch]);
-};
+                //store
+                console.log("other users -> ", res);
+                dispatch(setOtherUsers(res.data));
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchOtherUsers();
+    }, [])
+}
 
 export default useGetOtherUsers;
