cd5ab61dc7004b6bb03b7d044c561f8ca9fac814
Ranjeet <kiranjeetkr80@gmail.com>
2025-10-15 09:45:16 +0530

    done

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,57 +1,80 @@
-
 import { getReceiverSocketId, io } from "../index.js";
-import { Conversation } from "../models/conversationModel.js"; 
-import { Message } from "../models/messageModel.js"
+import { Conversation } from "../models/conversationModel.js";
+import { Message } from "../models/messageModel.js";
 
-export const sendMessage = async (req, res)=>{
-    try {
-        const senderId = req.id;
-        const receiverId = req.params.id;
-        const {message} = req.body;
+export const sendMessage = async (req, res) => {
+  try {
+    const senderId = req.id;
+    const receiverId = req.params.id;
+    const { message } = req.body;
 
-        let gotConversation = await Conversation.findOne({
-            participants:{$all :[senderId, receiverId]},
-        });
-        if(!gotConversation){
-            gotConversation = await Conversation.create({
-                participants:[senderId, receiverId]
-            });
-        };
-
-        const newMessage = await Message.create({
-            senderId,
-            receiverId,
-            message
-        });
-        if(newMessage){
-            gotConversation.messages.push(newMessage._id);
-        };
-        await Promise.all([gotConversation.save(), newMessage.save()]);
-
-        //Scoket Io
-        const receiverSocketId = getReceiverSocketId(receiverId);
-        if(receiverSocketId){
-            io.to(receiverSocketId).emit("newMessage", newMessage);
-        }
-        return res.status(201).json({
-            newMessage
-        });
-
-
-    } catch (error) {
-        console.log(error);
+    if (!message?.trim()) {
+      return res.status(400).json({ error: "Message cannot be empty" });
     }
-}
 
-export const getMessage = async(req, res)=>{
-    try {
-        const receiverId = req.params.id;
-        const senderId = req.id;
-        const conversation = await Conversation.findOne({
-            participants:{$all: [senderId, receiverId]}
-        }).populate("messages")
-        return res.status(200).json(conversation?.messages);
-    } catch (error) {
-        console.log(error);
+    // Check or create conversation
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
     }
-}
+
+    // Create new message
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    conversation.messages.push(newMessage._id);
+
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    const senderSocketId = getReceiverSocketId(senderId);
+    if (senderSocketId) {
+      io.to(senderSocketId).emit("newMessage", newMessage);
+    }
+
+    return res.status(201).json({
+      success: true,
+      newMessage,
+    });
+  } catch (error) {
+    console.error("Error in sendMessage:", error.message);
+    return res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const receiverId = req.params.id;
+    const senderId = req.id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    }).populate("messages");
+
+    if (!conversation) {
+      return res.status(200).json([]); 
+    }
+
+    return res.status(200).json(conversation.messages);
+  } catch (error) {
+    console.error("Error in getMessage:", error.message);
+    return res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+};
--- frontend/src/hooks/useGetRealTimeMessage.jsx
+++ frontend/src/hooks/useGetRealTimeMessage.jsx
@@ -1,16 +1,27 @@
 import { useEffect } from "react";
-import {useSelector, useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { setMessages } from "../redux/messageSlice";
 
 const useGetRealTimeMessage = () => {
-    const {socket} = useSelector(store=>store.socket);
-    const {messages} = useSelector(store=>store.message);
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        socket?.on("newMessage", (newMessage)=>{
-            dispatch(setMessages([...messages, newMessage]));
-        });
-        return () => socket?.off("newMessage");
-    },[setMessages, messages]);
+  const { socket } = useSelector((store) => store.socket);
+  const { messages } = useSelector((store) => store.message);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!socket) return;
+
+    const handleNewMessage = (newMessage) => {
+      // Append message instantly
+      dispatch(setMessages([...messages, newMessage]));
+    };
+
+    socket.on("newMessage", handleNewMessage);
+
+    // Cleanup
+    return () => {
+      socket.off("newMessage", handleNewMessage);
+    };
+  }, [socket, messages, dispatch]);
 };
+
 export default useGetRealTimeMessage;
